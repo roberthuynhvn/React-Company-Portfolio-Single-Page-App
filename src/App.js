f3e2cb950133626e513b1bb87cebbd02846882d4
Thach Huynh <68765864+roberthuynhvn@users.noreply.github.com>
2023-02-12 12:28:41 +0700

    Adding navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <h1>Hello react js. We created project</h1>;
+  return <h1>Hello react js. We will add navbar.</h1>;
 };
 
 export default App;
